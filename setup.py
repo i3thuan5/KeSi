4f62426f87f6efb6221c677cb63cd69e5cd8f775
a8568730 <a8568730@hotmail.com>
2020-01-30 15:54:48 +0800

    + Tox kah travis CI

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from panpun import PANPUN
+
+
+setup(
+    name='KeSi',
+    description='台語文NLP家私',
+    long_description='Kā台文轉做電腦通利用ê格式。臺灣言語工具翻新。',
+    packages=['kesi'],
+    version=PANPUN,
+    author='Tshuà Bûn-lī',
+    author_email='ithuan@ithuan.tw',
+    url='https://ithuan.tw/',
+    download_url='https://github.com/i3thuan5/KeSi',
+    keywords=[
+        'Parser', 'Alignment', 'Taigi', 'Hanji', 'Lomaji',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+)
