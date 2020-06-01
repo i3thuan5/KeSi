34d432e486ff6113bafa77613699d783d0b03515
a8568730 <a8568730@hotmail.com>
2020-06-01 14:28:56 +0800

    * Jī-á tī init ài kiám-tsa jī ū khin-siann--bô.

--- kesi/butkian/ji.py
+++ kesi/butkian/ji.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 from kesi.susia.POJ import tsuanPOJ
 from kesi.susia.TL import tsuanTL
+from kesi.butkian.kongiong import KHIN_SIANN_HU
 
 
 class Ji:
 
     def __init__(self, hanlo, lomaji=None, si_khinsiann=False):
-        print('init ji.hanlo=', hanlo)
+        print('init ji.hanlo={}, si_khinsiann={}'.format(hanlo, si_khinsiann))
         if si_khinsiann:
             self.hanlo = '--{}'.format(hanlo)
         else:
+            if hanlo.startswith(KHIN_SIANN_HU):
+                si_khinsiann = True
             self.hanlo = hanlo
 
         if lomaji and si_khinsiann:
@@ -21,8 +24,17 @@ class Ji:
         self.si_khinsiann = si_khinsiann
 
     def POJ(self):
+        print('>> POJ <<')
+        print('hanlo of ji.POJ() at first=', self.hanlo)
+        print('self.si_khinsiann=', self.si_khinsiann)
+        if self.si_khinsiann:
+            hanlo = self.hanlo[2:]
+            lomaji = self.lomaji[2:]
+        else:
+            hanlo = self.hanlo
+            lomaji = self.lomaji
         return Ji(
-            tsuanPOJ(self.hanlo), tsuanPOJ(self.lomaji),
+            tsuanPOJ(hanlo), tsuanPOJ(lomaji),
             si_khinsiann=self.si_khinsiann
         )
 
