53bd24eef63c0e6f09030fba57a0e1e2c8a37964
niauah <fernandochen799@gmail.com>
2020-06-10 17:07:59 +0800

    jisoo.py flake8

--- le/sng_jisoo.py
+++ le/sng_jisoo.py
@@ -1,12 +1,13 @@
 from kesi import Ku
 import sys
 
+
 def sng():
     jisoo = 0
     for line in sys.stdin:
         jisoo += len(list(Ku(line.rstrip()).thianji()))
     print('字數=', jisoo)
 
+
 if __name__ == '__main__':
     sng()
-
